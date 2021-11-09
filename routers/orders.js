6c972db16c18eb817ed9d4acec6d1df64e0a04c8
basco1979 <el_basco@hotmail.com>
2021-11-09 10:50:05 -0300

    v1.2

--- routers/orders.js
+++ routers/orders.js
@@ -1,57 +1,143 @@
-const express = require('express');
-const router = express.Router();
-const {Order} = require('../models/order');
-const {OrderItem} = require('../models/orderItem');
+const express = require('express')
+const router = express.Router()
+const { Order } = require('../models/order')
+const { OrderItem } = require('../models/orderItem')
 
 
 router.get('/', async (req, res) => {
-    const orders = await Order.find().populate('user', 'name').sort({'dateOrdered' : -1});
-    if(!orders){
-        res.status(500).json({success:false})
+    const orders = await Order.find()
+        .populate('user', 'name')
+        .sort({ dateOrdered: -1 })
+    if (!orders) {
+        res.status(500).json({ success: false })
     }
-    res.send(orders);
+    res.send(orders)
 })
 
 router.get('/:id', async (req, res) => {
     const order = await Order.findById(req.params.id)
-    .populate('user', 'name')
-    .populate({path : 'orderItem', populate : {path : 'product', populate : 'category'}});
-    if(!order){
-        res.status(500).json({success:false})
+        .populate('user', 'name')
+        .populate({
+            path: 'orderItem',
+            populate: { path: 'product', populate: 'category' },
+        })
+    if (!order) {
+        res.status(500).json({ success: false })
     }
-    res.send(order);
+    res.send(order)
 })
 
 router.post('/', async (req, res) => {
+    const orderItemIds = Promise.all(
+        req.body.orderItem.map(async (orderitem) => {
+            let newOrderItem = new OrderItem({
+                quantity: orderitem.quantity,
+                product: orderitem.product,
+            })
 
-    const orderItemIds = Promise.all(req.body.orderItem.map(async(orderitem)=>{
-        let newOrderItem = new OrderItem({
-            quantity : orderitem.quantity,
-            product : orderitem.product
+            newOrderItem = await newOrderItem.save()
+
+            return newOrderItem._id
         })
+    )
 
-        newOrderItem = await newOrderItem.save();
+    const orderItemsIdsResolved = await orderItemIds
+    const totalPrices = await Promise.all(
+        orderItemsIdsResolved.map(async (orderItemId) => {
+            const orderItem = await OrderItem.findById(orderItemId).populate(
+                'product',
+                'price'
+            )
+            const totalPrice = orderItem.product.price * orderItem.quantity
+            return totalPrice
+        })
+    )
+    const totalPrice = totalPrices.reduce((a, b) => a + b, 0)
 
-        return newOrderItem._id;
-
-    }))
-
-    const orderItemsIdsResolved = await orderItemIds;
     let order = new Order({
-        orderItem : orderItemsIdsResolved,
-        shippingAddress1 : req.body.shippingAddress1,
-        shippingAddress2 : req.body.shippingAddress2,
-        city : req.body.city,
-        zip : req.body.zip,
-        country : req.body.country,
-        phone : req.body.phone,
-        status : req.body.status,
-        totalPrice : req.body.totalPrice,
-        user : req.body.user
+        orderItem: orderItemsIdsResolved,
+        shippingAddress1: req.body.shippingAddress1,
+        shippingAddress2: req.body.shippingAddress2,
+        city: req.body.city,
+        zip: req.body.zip,
+        country: req.body.country,
+        phone: req.body.phone,
+        status: req.body.status,
+        totalPrice: totalPrice,
+        user: req.body.user,
     })
     order = await order.save()
     if (!order) return res.status(404).send('the order connot be created')
     res.send(order)
 })
 
-module.exports = router;
+router.put('/:id', async (req, res) => {
+    const order = await Order.findByIdAndUpdate(
+        req.params.id,
+        {
+            status: req.body.status,
+        },
+        { new: true }
+    )
+    if (!order) return res.status(404).send('the order connot be updated')
+    res.send(order)
+})
+
+router.delete('/:id', (req, res) => {
+    Order.findByIdAndRemove(req.params.id)
+        .then(async (order) => {
+            if (order) {
+                await order.orderItem.map(async (orderIt) => {
+                    await OrderItem.findByIdAndRemove(orderIt)
+                })
+                return res.status(200).json({
+                    success: true,
+                    meessage: 'the order is deleted',
+                })
+            } else {
+                return res
+                    .status(404)
+                    .json({ success: false, message: 'order not found' })
+            }
+        })
+        .catch((err) => {
+            return res.status(400).json({ success: false, error: err })
+        })
+})
+
+router.get('/get/totalSales', async (req, res) => {
+    const totalSales = await Order.aggregate([
+        { $group: { _id: null, totalSales: { $sum: '$totalPrice' } } },
+    ])
+    if (!totalSales) {
+        return res.status(400).send('The order sales cannot be generated')
+    }
+    res.send({ totalSales: totalSales.pop().totalSales })
+})
+
+router.get('/get/count', async (req, res) => {
+    const ordersCount = await Order.countDocuments()
+    if (!ordersCount) {
+        res.status(500).json({ success: false })
+    }
+    res.send({
+        ordersCount: ordersCount,
+    })
+})
+
+router.get('/get/userOrders/:userId', async (req, res) => {
+    const userOrders = await Order.find({ user: req.params.userId })
+        .populate({
+            path: 'orderItem',
+            populate: { path: 'product', populate: 'category' },
+        })
+        .sort({ dateOrdered: -1 })
+    if (!userOrders) {
+        res.status(500).json({ success: false })
+    }
+    res.send(userOrders)
+})
+
+
+
+module.exports = router
--- routers/products.js
+++ routers/products.js
@@ -1,9 +1,34 @@
-const express = require('express');
-const { Category } = require('../models/category');
-const router = express.Router();
-const {Product} = require('../models/product');
-const mongoose = require('mongoose');   
+const express = require('express')
+const { Category } = require('../models/category')
+const router = express.Router()
+const { Product } = require('../models/product')
+const mongoose = require('mongoose')
 
+const multer = require('multer')
+
+const FILE_TYPE_MAP = {
+    'image/png': 'png',
+    'image/jpeg': 'jpeg',
+    'image/jpg': 'jpg',
+}
+
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+        const validFile = FILE_TYPE_MAP[file.mimetype]
+        let uploadError = new Error('Invalid image type')
+        if (validFile) {
+            uploadError = null
+        }
+        cb(uploadError, 'public/uploads')
+    },
+    filename: function (req, file, cb) {
+        const fileName = file.originalname.split(' ').join('-')
+        const extension = FILE_TYPE_MAP[file.mimetype]
+        cb(null, `${fileName}-${Date.now()}.${extension}`)
+    },
+})
+
+const uploadOptions = multer({ storage: storage })
 
 /* router.get('/', async (req, res) => {
     const products = await Product.find().populate('category');
@@ -14,90 +39,109 @@ const mongoose = require('mongoose');
 }) */
 
 router.get('/:id', async (req, res) => {
-    const product = await Product.findById(req.params.id).populate('category');
-    if(!product){
-        res.status(500).json({success:false})
+    const product = await Product.findById(req.params.id).populate('category')
+    if (!product) {
+        res.status(500).json({ success: false })
     }
-    res.send(product);
+    res.send(product)
 })
-
 
 //filtrar con query params
 router.get('/', async (req, res) => {
-    let filter = {};
-    if(req.query.categories){
-        filter = {category : req.query.categories}
+    let filter = {}
+    if (req.query.categories) {
+        filter = { category: req.query.categories }
     }
 
-    const products = await Product.find(filter).populate('category');
-    if(!products){
-        res.status(500).json({success:false})
+    const products = await Product.find(filter).populate('category')
+    if (!products) {
+        res.status(500).json({ success: false })
     }
-    res.send(products);
+    res.send(products)
 })
 
+router.post('/', uploadOptions.single('image'), async (req, res) => {
+    const category = await Category.findById(req.body.category)
+    if (!category) return res.status(400).send('Invalid category')
 
-router.post('/', async (req, res) => {
-    const category = await Category.findById(req.body.category);
-    if(!category)return res.status(400).send('Invalid category');
-
+    const file = req.file
+    if (!file) return res.status(400).send('No image in the request')
+    const fileName = req.file.filename
+    const basePath = `${req.protocol}://${req.get(
+        'host'
+    )}/backend/public/uploads`
     let product = new Product({
-        name : req.body.name,
-        description : req.body.description,
-        richDescription : req.body.richDescription,
-        brand : req.body.brand, 
-        image : req.body.image,
-        price : req.body.price,
-        category : req.body.category,
-        countInStock : req.body.countInStock,
-        rating : req.body.rating,
-        numReviews : req.body.numReviews,
-        isFeatured : req.body.isFeatured,
+        name: req.body.name,
+        description: req.body.description,
+        richDescription: req.body.richDescription,
+        brand: req.body.brand,
+        image: `${basePath}${fileName}`,
+        price: req.body.price,
+        category: req.body.category,
+        countInStock: req.body.countInStock,
+        rating: req.body.rating,
+        numReviews: req.body.numReviews,
+        isFeatured: req.body.isFeatured,
     })
-    
-    product = await product.save();
-    if(!product) return res.status(500).send("The product cannot be created");
-    
-    res.send(product);
+
+    product = await product.save()
+    if (!product) return res.status(500).send('The product cannot be created')
+
+    res.send(product)
 })
 
-router.put('/:id', async(req, res)=>{
-    if(!mongoose.isValidObjectId(req.params.id)){
+router.put('/:id', uploadOptions.single('image'), async (req, res) => {
+    if (!mongoose.isValidObjectId(req.params.id)) {
         res.status(400).send('Invalid Product Id')
     }
-    const category = await Category.findById(req.body.category);
-    if(!category)return res.status(400).send('Invalid category');
+    const category = await Category.findById(req.body.category)
+    if (!category) return res.status(400).send('Invalid category')
 
-    const product = await Product.findByIdAndUpdate(
+    const product = await Product.findById(req.params.id)
+    if (!product) return res.status(400).send('Invalid product')
+
+    const file = req.file
+    let imagepath
+    if (file) {
+        const fileName = req.file.filename
+        const basePath = `${req.protocol}://${req.get(
+            'host'
+        )}/backend/public/uploads`
+        imagepath = `${basePath}${fileName}`
+    }
+    else {
+        imagepath = product.image;
+    }
+
+    const updatedProduct = await Product.findByIdAndUpdate(
         req.params.id,
         {
-            name : req.body.name,
-            description : req.body.description,
-            richDescription : req.body.richDescription,
-            brand : req.body.brand, 
-            image : req.body.image,
-            price : req.body.price,
-            category : req.body.category,
-            countInStock : req.body.countInStock,
-            rating : req.body.rating,
-            numReviews : req.body.numReviews,
-            isFeatured : req.body.isFeatured,        },
-        {new: true}
+            name: req.body.name,
+            description: req.body.description,
+            richDescription: req.body.richDescription,
+            brand: req.body.brand,
+            image: imagepath,
+            price: req.body.price,
+            category: req.body.category,
+            countInStock: req.body.countInStock,
+            rating: req.body.rating,
+            numReviews: req.body.numReviews,
+            isFeatured: req.body.isFeatured,
+        },
+        { new: true }
     )
-    if (!product) return res.status(404).send('the product connot be updated')
-    res.send(product)
+    if (!updatedProduct) return res.status(404).send('the product connot be updated')
+    res.send(updatedProduct)
 })
 
 router.delete('/:id', (req, res) => {
     Product.findByIdAndRemove(req.params.id)
         .then((product) => {
             if (product) {
-                return res
-                    .status(200)
-                    .json({
-                        success: true,
-                        meessage: 'the product is deleted',
-                    })
+                return res.status(200).json({
+                    success: true,
+                    meessage: 'the product is deleted',
+                })
             } else {
                 return res
                     .status(404)
@@ -110,39 +154,64 @@ router.delete('/:id', (req, res) => {
 })
 
 router.get('/get/count', async (req, res) => {
-    const productCount = await Product.countDocuments();
-    if(!productCount){
-        res.status(500).json({success:false})
+    const productCount = await Product.countDocuments()
+    if (!productCount) {
+        res.status(500).json({ success: false })
     }
-    res.send(
-        {
-            productCount : productCount
-        }
-        );
+    res.send({
+        productCount: productCount,
+    })
 })
 
 router.get('/get/featured/', async (req, res) => {
-
     const productFeatured = await Product.find({
-        isFeatured : true
-    });
-    if(!productFeatured){
-        res.status(500).json({success:false})
+        isFeatured: true,
+    })
+    if (!productFeatured) {
+        res.status(500).json({ success: false })
     }
     res.send(productFeatured)
 })
 
 //limitar la cantidad de featured
 router.get('/get/featured/:count', async (req, res) => {
-    const count = req.params.count ? req.params.count : 0;
+    const count = req.params.count ? req.params.count : 0
 
     const productFeatured = await Product.find({
-        isFeatured : true
-    }).limit(+count);
-    if(!productFeatured){
-        res.status(500).json({success:false})
+        isFeatured: true,
+    }).limit(+count)
+    if (!productFeatured) {
+        res.status(500).json({ success: false })
     }
     res.send(productFeatured)
 })
 
-module.exports = router;
+
+//Para subir varias imagenes
+router.put('/gallery-images/:id', 
+uploadOptions.array('images', 10), 
+async (req, res) => {
+    if (!mongoose.isValidObjectId(req.params.id)) {
+        res.status(400).send('Invalid Product Id')
+    }
+    const files = req.files
+    let imagepaths = [];
+    const basePath = `${req.protocol}://${req.get(
+        'host'
+    )}/backend/public/uploads`;
+    if(files){
+        files.map(file=>{
+            imagepaths.push(`${basePath}${file.fileName}`)
+        })
+    }
+    const product = await Product.findByIdAndUpdate(
+        req.params.id,
+        {
+            images: imagepaths,
+        },
+        { new: true }
+    )
+    if (!product) return res.status(404).send('the product connot be updated')
+    res.send(product)
+})
+module.exports = router
